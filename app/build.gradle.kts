ecb6dc75fc02c1956712456574ffcb56a0ba80f2
SkyKai <1132234509@qq.com>
2026-03-14 21:27:12 +0800

    feat: update versionCode & versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.vibe.app"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
