bd9752ba9eaf5e456b6471cb9a783cadbf05d389
SkyKai <1132234509@qq.com>
2026-03-21 08:33:03 +0800

    update icon

--- app/src/main/kotlin/com/vibe/app/presentation/ui/chat/ChatBubble.kt
+++ app/src/main/kotlin/com/vibe/app/presentation/ui/chat/ChatBubble.kt
@@ -179,9 +179,9 @@ fun GPTMobileIcon(loading: Boolean) {
             )
         }
         Image(
-            painter = painterResource(R.drawable.ic_gpt_mobile_no_padding),
+            painter = painterResource(R.drawable.ic_vibe),
             contentDescription = null,
-            modifier = Modifier.size(24.dp)
+            modifier = Modifier.size(36.dp)
         )
     }
 }
